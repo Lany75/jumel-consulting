a46c8c7a959f20b4f588948797dd6850e9cb6582
Lany75 <mlanie.parry@gmail.com>
2020-10-09 17:46:14 +0200

    component Home.js

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div id="home">
+      <div id="header">Header</div>
+      <div id="nom-entreprise">JUMEL CONSULTING</div>
+    </div>
+  );
+};
+
+export default Home;
