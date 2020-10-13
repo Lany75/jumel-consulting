6eb709f1a88e7adfc6881b7264b6e6883a6b0eaf
Lany75 <mlanie.parry@gmail.com>
2020-10-13 16:02:21 +0200

    création ancres du menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,11 +4,21 @@ import "./MenuDesktop.css";
 const Menu = () => {
   return (
     <div id="menu">
-      <div className="div-menu">Accueil</div>
-      <div className="div-menu">A propos</div>
-      <div className="div-menu">Compétences</div>
-      <div className="div-menu">Portfolio</div>
-      <div className="div-menu">Contact</div>
+      <div className="div-menu">
+        <a href="#home">Accueil</a>
+      </div>
+      <div className="div-menu">
+        <a href="#about">A propos</a>
+      </div>
+      <div className="div-menu">
+        <a href="#skills">Compétences</a>
+      </div>
+      <div className="div-menu">
+        <a href="#portfolio">Portfolio</a>
+      </div>
+      <div className="div-menu">
+        <a href="#contact">Contact</a>
+      </div>
     </div>
   );
 };
