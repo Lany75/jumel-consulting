a3a835cc120a38f092767c3837fe3b3757b504e9
Lany75 <mlanie.parry@gmail.com>
2020-10-20 15:59:34 +0200

    modif portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -21,15 +21,22 @@ const Portfolio = () => {
           <div>ENGIE Global Markets</div>
         </div>
         <div className="poste-experience">Technical Lead</div>
-        <div className="mission-experience">
+        {/*<div className="mission-experience">
           <ul>
             <li>Conception et Développement d'un ensemble de librairies .NET pour la mise en place de microservices et d'API REST</li>
             <li>Conception et Développement d'un ETL pour la collecte et l'ingestion de données vers un agrégateur multidimensionnel OLAP</li>
             <li>Mise en place de pipeline CI/CD sur Azure DevOps</li>
-            <li>Environnement technique : C#, WPF, REST, WebAPI, DevExpress, MongoDB, Azure DevOps, Datalake, ETL, RabbitMQ</li>
-            <li>Environnement fonctionnel : VaR, PnL, Risk Analysis</li>
           </ul>
-        </div>
+          <div className="environnement-technique">
+            <div id="titre-env-tech">Environnement technique</div>
+            <div id="comp-env-tech">C#, WPF, REST, WebAPI, DevExpress, MongoDB, Azure DevOps, Datalake, ETL, RabbitMQ</div>
+          </div>
+          <div className="environnement-fonctionnel">
+            <div id="titre-env-fonct">Environnement fonctionnel</div>
+            <div id="comp-env-fonct"> VaR, PnL, Risk Analysis</div>
+          </div>
+        </div>*/
+  }
       </div>
       <div id="experience-2">
         <div className="entreprise-experience">
@@ -37,7 +44,7 @@ const Portfolio = () => {
           <div>AXA Life Invest</div>
         </div>
         <div className="poste-experience">Senior Software Engineer Team Lead</div>
-        <div className="mission-experience">
+        {/*<div className="mission-experience">
           <ul>
             <li>Développement d'une plateforme de gestion de produits d'assurance vie en .NET</li>
             <li>Mise en place d'une plateforme de continuous delivery</li>
@@ -48,14 +55,15 @@ const Portfolio = () => {
             <li>Environnement fonctionnel : Assurance, ICPPI</li>
           </ul>
         </div>
-      </div>
+*/}
+        </div>
       <div id="experience-3">
         <div className="entreprise-experience">
           <div><img className="logo-entreprise" src={LogoSG} alt="Société Générale" /></div>
           <div>Societe Generale Corporate and Investment Banking</div>
         </div>
         <div className="poste-experience">Technical Lead</div>
-        <div className="mission-experience">
+      {/*}  <div className="mission-experience">
           <ul>
             <li>Développement et maintenance évolutive sur la plateforme de deal lifecycle dédiée aux options de changes Enterprise</li>
             <li>Mise en place d'un bridge temps réel entre XOne et Enterprise</li>
@@ -64,6 +72,7 @@ const Portfolio = () => {
             <li>Environnement fonctionnel : Options de change, Pricing intéractif, Tenue de position, Deal Lifecycle, Produits Exotiques, Produits Structurés</li>
           </ul>
         </div>
+     */}
       </div>
       </div>
       <div className="experiences">
@@ -73,7 +82,7 @@ const Portfolio = () => {
           <div>Amundi</div>
         </div>
         <div className="poste-experience">Senior J2Ee Developer</div>
-        <div className="mission-experience">
+       {/* <div className="mission-experience">
           <ul>
             <li>Développement et maintenance évolutive sur les modules Credit Risk de l'application Media+</li>
             <li>Conception et Développement du module de scoring ISR de l'application Media+</li>
@@ -81,6 +90,7 @@ const Portfolio = () => {
             <li>Environnement fonctionnel : Asset Management, Risque de crédit, ISR</li>
           </ul>
         </div>
+    */}
       </div>
       <div id="experience-5">
         <div className="entreprise-experience">
@@ -88,13 +98,14 @@ const Portfolio = () => {
           <div>Atos</div>
         </div>
         <div className="poste-experience">Information Technology Software Engineer</div>
-        <div className="mission-experience">
+       {/* <div className="mission-experience">
           <ul>
             <li>Conception technique en UML de l'application d'attribution des aides à l'AESN</li>
             <li>Développement d'une application n-tiers J2EE avec une approche pilotée par le modèle (MDA)</li>
             <li>Environnement technique : J2EE, Axis, Web-Services, Spring, Hibernate, Struts, Maven, Subversion, CruiseControl, Oracle</li>
           </ul>
         </div>
+      */}
       </div>
       </div>
     </div>
