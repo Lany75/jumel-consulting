747f9ea74da9a57dc2d114cfa9c8d82c7a91655f
Lany75 <mlanie.parry@gmail.com>
2020-10-20 17:40:11 +0200

    complément composant about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { CgArrowUpO } from "react-icons/cg";
+import consulting from "../../images/consulting.jpg";
+import bfa from "../../images/BFA.jpg";
+import cloud from "../../images/cloud.jpg";
 import "./AboutDesktop.css";
 import "./AboutMobile.css";
 
 const About = () => {
   return (
     <div id="about">
-      <div id="titre-about">A Propos</div>
-      <div id="text-about"> lorem ipsum</div>
-      <div>
-        <a href="#home" className="icon-retour">
-          <CgArrowUpO />
-        </a>
+      <div id="titre-about">Expertise</div>
+      <div id="text-about">
+        <div>
+          <img  className="image-about" src={consulting} alt="consulting" />
+          <div>lorem ipsum</div>
+        </div>
+        <div>
+          <img  className="image-about" src={bfa} alt="banque-finance-assurance" />
+        <div>lorem ipsum</div>
+        </div>
+        <div>
+          <img  className="image-about" src={cloud} alt="cloud" />
+        <div>lorem ipsum</div>
+        </div>
+      </div>
+      
+      <div className="icon-retour">
+        <a href="#home"><CgArrowUpO /></a>
       </div>
     </div>
   );
