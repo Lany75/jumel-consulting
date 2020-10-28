deecb9d6348c43abed0e7a6e957889e2305f2b3d
Lany75 <mlanie.parry@gmail.com>
2020-10-28 15:56:37 +0100

    test Portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -15,44 +15,54 @@ const Portfolio = () => {
   <div id="portfolio">
     <div id="text-portfolio">
       <div className="experiences">
-        <Experience 
-          idExperience="experience-1" 
-          logoEntreprise={logoEngie} 
-          altImage="Engie" 
-          nomEntreprise="ENGIE Global Markets" 
-          titrePoste="Technical Lead"
-        />
-        <Experience 
-          idExperience="experience-2" 
-          logoEntreprise={LogoAxa} 
-          altImage="Axa" 
-          nomEntreprise="AXA Life Invest" 
-          titrePoste="Senior Software Engineer Team Lead"
-        />
-        <Experience 
-          idExperience="experience-3" 
-          logoEntreprise={LogoSG} 
-          altImage="Société Générale" 
-          nomEntreprise="Societe Generale Corporate and Investment Banking" 
-          titrePoste="Technical Lead"
-        />
+        <div className="experience">
+          <Experience 
+            idExperience="experience-1" 
+            logoEntreprise={logoEngie} 
+            altImage="Engie" 
+            nomEntreprise="ENGIE Global Markets" 
+            titrePoste="Technical Lead"
+          />
+        </div> 
+        <div className="experience">
+          <Experience 
+            idExperience="experience-2" 
+            logoEntreprise={LogoAxa} 
+            altImage="Axa" 
+            nomEntreprise="AXA Life Invest" 
+            titrePoste="Senior Software Engineer Team Lead"
+          />
+        </div>
+        <div className="experience">
+          <Experience 
+            idExperience="experience-3" 
+            logoEntreprise={LogoSG} 
+            altImage="Société Générale" 
+            nomEntreprise="Societe Generale Corporate and Investment Banking" 
+            titrePoste="Technical Lead"
+          />
+        </div>
       </div>
 
       <div className="experiences">
-        <Experience 
-          idExperience="experience-4" 
-          logoEntreprise={LogoAmundi} 
-          altImage="Amundi" 
-          nomEntreprise="Amundi" 
-          titrePoste="Senior J2Ee Developer"
-        />
-        <Experience 
-          idExperience="experience-5" 
-          logoEntreprise={LogoAtos} 
-          altImage="Atos" 
-          nomEntreprise="Atos" 
-          titrePoste="Information Technology Software Engineer"
-        />
+        <div className="experience">
+          <Experience 
+            idExperience="experience-4" 
+            logoEntreprise={LogoAmundi} 
+            altImage="Amundi" 
+            nomEntreprise="Amundi" 
+            titrePoste="Senior J2Ee Developer"
+          />
+        </div>
+        <div className="experience">
+          <Experience 
+            idExperience="experience-5" 
+            logoEntreprise={LogoAtos} 
+            altImage="Atos" 
+            nomEntreprise="Atos" 
+            titrePoste="Information Technology Software Engineer"
+          />
+        </div>
       </div>
     </div>
 
