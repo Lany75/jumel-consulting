c83081d2c462d4e71dc10ffd543aa2f9aef13f72
Lany75 <mlanie.parry@gmail.com>
2020-11-05 09:42:41 +0100

    création css composant Experience

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './ExperienceDesktop.css';
+import './ExperienceMobile.css';
 
 const Experience = ({idExperience, logoEntreprise, altImage, nomEntreprise, titrePoste}) => {
   return (
