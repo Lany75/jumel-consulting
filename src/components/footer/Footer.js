9e91e06ab5fb18d23a1eedb34067787b5194effd
Lany75 <mlanie.parry@gmail.com>
2020-11-05 11:36:40 +0100

    début développement footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,24 @@ import './FooterDesktop.css';
 import './FooterMobile.css';
 
 const Footer = () => {
-  return <div id='footer'>Footer</div>
+  return (
+    <div id='footer'>
+      <div id='rappel-menu'>
+        <div>
+          <a href="#transition-1" className="rappel-menu">Expertise</a>
+        </div>
+        <div>
+          <a href="#transition-2" className="rappel-menu">Compétences</a>
+        </div>
+        <div>
+          <a href="#transition-3" className="rappel-menu">Expériences</a>
+        </div>
+      </div>
+      <div id='mention-legales'>
+        Mentions Légales
+      </div>
+    </div>
+  )
 }
 
 export default Footer;
